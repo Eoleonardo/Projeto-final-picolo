c5bae3f25f6bfa15f537723d583b1e453ee829f9
Eo.leo silva <101286236+Eoleonardo@users.noreply.github.com>
2023-05-29 14:01:09 -0400

    Commit inicial

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://cqihkpju:U-ObT8iUvQ_vQy6qu1c0dmwWTsZ-epmZ@silly.db.elephantsql.com/cqihkpju'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
